e9344b97602db9d1a515fa4b6d84c6c786508280
shyam <shyamprabhath16@gmail.com>
2026-05-05 16:41:25 +0530

    Add required dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { FollowModule } from './modules/follow/follow.module';
+import { PostModule } from './modules/post/post.module';
+import { LikeModule } from './modules/like/like.module';
+import { CommentModule } from './modules/comment/comment.module';
+import { FeedModule } from './modules/feed/feed.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+     AuthModule,
+     UserModule,
+     FollowModule,
+     PostModule,
+     LikeModule,
+     CommentModule,
+     FeedModule,
+  ],
 })
 export class AppModule {}
