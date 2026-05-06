9a90098166c39c6207cb39ccb41b0663eff67069
shyam <shyamprabhath16@gmail.com>
2026-05-06 21:58:05 +0530

    Update profile route with custom user decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { SignupDto } from './dto/signup.dto.js';
 import { LoginDto } from './dto/login.dto.js';
@@ -6,6 +6,7 @@ import { ForgotPasswordDto } from './dto/forgot-password.dto.js';
 import { ResetPasswordDto } from './dto/reset-password.dto.js';
 import { IdentityValidationPipe } from '../../common/pipes/identity-validation.pipe.js';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard.js';
+import { CurrentUser } from '../../common/decorators/current-user.decorator.js';
 
 @Controller('auth')
 export class AuthController {
@@ -24,8 +25,8 @@ export class AuthController {
 
     @Get('profile')
     @UseGuards(JwtAuthGuard)
-    getProfile(@Req() req) {
-        return req.user;
+    getProfile(@CurrentUser() user:any) {
+        return user;
     }
     
     @Post('forgot-password')
